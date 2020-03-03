5b94c9267a28174b40ebd96fdfcbd22300c2de8f
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-03-03 12:31:09 -0500

    move CompressionDummyFactory to new Compression.Test project

---
+++ OBeautifulCode.Compression.Test/CompressionDummyFactoryTest.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompressionDummyFactoryTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Compression.Test
+{
+    using System;
+
+    using FakeItEasy;
+
+    using OBeautifulCode.AutoFakeItEasy;
+
+    using Xunit;
+
+    public static partial class CompressionDummyFactoryTest
+    {
+        /* Add your Dummy Factory tests, if any, here. */
+    }
+}
